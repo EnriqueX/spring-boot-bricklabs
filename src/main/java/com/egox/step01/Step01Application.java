4ffa4bf8854ea2a7e3f09e10552da7c4234c5cd3
Enrique Gonzalez <aegonzalez@dextratech.com>
2023-04-14 14:04:16 -0600

    Implement localization

--- src/main/java/com/egox/step01/Step01Application.java
+++ src/main/java/com/egox/step01/Step01Application.java
@@ -2,12 +2,32 @@ package com.egox.step01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Step01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Step01Application.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource resourceBundleMessageSource(){
+		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("messages");
+		return resourceBundleMessageSource;
 	}
 
 }
--- src/main/java/com/egox/step01/controllers/HelloWorldController.java
+++ src/main/java/com/egox/step01/controllers/HelloWorldController.java
@@ -1,14 +1,21 @@
 package com.egox.step01.controllers;
 
 import com.egox.step01.models.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 
     //URI - /helloWorld
     //GET Method
@@ -25,5 +32,15 @@ public class HelloWorldController {
         ud.setLastName("X");
         ud.setCity("Monterrey");
         return ud;
+    }
+
+    @GetMapping("/getMessages")
+    public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-language", required = false) String locale){
+        return messageSource.getMessage("label.hello", null, Locale.of(locale));
+    }
+
+    @GetMapping("/getMessages2")
+    public String getMessagesInI18NFormat2(){
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
     }
 }
